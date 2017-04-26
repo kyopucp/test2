5107043fcd3a869db194b5e57ae9f7168638769e
pejcairampoma <pejcairampoma@PELT0108573.galileo.ebel>
2017-04-26 18:39:47 -0500

    first commits

---
+++ test2/src/jc/Test2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jc;
+
+/**
+ * @author pejcairampoma
+ *
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// INITIAL
+		//
+	}
+
+}
